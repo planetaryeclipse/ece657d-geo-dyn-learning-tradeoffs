4f86f9f19f0315c6d7d8653660d8d300e3c01e50
Samuel Street <planetaryeclipse@gmail.com>
2026-03-10 19:21:28 -0400

    Add metric and christoffels for hypersphere

--- src/trajectory/sn_mfld.py
+++ src/trajectory/sn_mfld.py
@@ -1,66 +1,89 @@
 # Sn manifold (n-dimensional hypersphere smoothly embedded in Rn+1)
 from math import prod
 
-import numpy as np
+import torch
+
+from torch.func import grad
+from torch.autograd.functional import jacobian
+
 import itertools
 
 from typing import List
 
 
-def to_intrinsic(euclid: np.ndarray) -> np.ndarray:
+def to_intrinsic(euclid: torch.Tensor) -> torch.Tensor:
     euclid_n = euclid.shape[0]  # dimension of the ambient Euclidean space
     if euclid_n < 2:
         raise ValueError("Euclidean dimension must be >= 2")
 
     n = euclid_n - 1
 
-    intrinsic = np.zeros((n,))
+    intrinsic = torch.zeros((n,))
 
     if n == 1:
-        intrinsic[0] = np.atan2(euclid[1], euclid[0])
+        intrinsic[0] = torch.atan2(euclid[1], euclid[0])
     else:
-        intrinsic[0] = np.acos(euclid[0])
-        cum_prod = np.sin(intrinsic[0])
+        intrinsic[0] = torch.acos(euclid[0])
+        cum_prod = torch.sin(intrinsic[0])
 
         for i in range(1, n - 1):
-            intrinsic[i] = np.acos(euclid[i] / cum_prod)
-            cum_prod *= np.sin(intrinsic[i])
-        intrinsic[-1] = np.atan2(euclid[-2] / cum_prod, euclid[-1]/cum_prod)
+            intrinsic[i] = torch.acos(euclid[i] / cum_prod)
+            cum_prod = cum_prod * torch.sin(intrinsic[i])  # re-assigned to prevent autograd error when differentiating
+        intrinsic[-1] = torch.atan2(euclid[-2] / cum_prod, euclid[-1] / cum_prod)
     return intrinsic
 
 
-def to_extrinsic(intrinsic: np.ndarray) -> np.ndarray:
+def to_extrinsic(intrinsic: torch.Tensor) -> torch.Tensor:
     n = intrinsic.shape[0]
-    euclid = np.zeros((n + 1,))
+    euclid = torch.zeros((n + 1,))
 
     if n == 1:  # implying extrinsic is at least dim 2
-        euclid[0] = np.cos(intrinsic[0])
-        euclid[1] = np.sin(intrinsic[0])
+        euclid[0] = torch.cos(intrinsic[0])
+        euclid[1] = torch.sin(intrinsic[0])
     else:  # implying extrinsic is at least dim 3
-        euclid[0] = np.cos(intrinsic[0])
-        cum_prod = np.sin(intrinsic[0])
+        euclid[0] = torch.cos(intrinsic[0])
+        cum_prod = torch.sin(intrinsic[0])
         for i in range(1, n - 1):
-            euclid[i] = np.cos(intrinsic[i]) * cum_prod
-            cum_prod *= np.sin(intrinsic[i])
-        euclid[-2] = np.sin(intrinsic[-1]) * cum_prod
-        euclid[-1] = np.cos(intrinsic[-1]) * cum_prod
+            euclid[i] = torch.cos(intrinsic[i]) * cum_prod  # re-assigned
+            cum_prod = cum_prod * torch.sin(intrinsic[i])
+        euclid[-2] = torch.sin(intrinsic[-1]) * cum_prod
+        euclid[-1] = torch.cos(intrinsic[-1]) * cum_prod
 
     return euclid
 
 
-def _switch_antipodal_coords(coords: np.ndarray, switch_coords: List[bool]) -> np.ndarray:
-    continuous_coords = (coords + 2 * np.pi) - np.array([np.pi if switch else 0 for switch in switch_coords])
-    switched_coords = np.array([coord if coord <= np.pi else -(2 * np.pi - coord) for coord in continuous_coords])
+def _switch_antipodal_coords(coords: torch.ndarray, switch_coords: List[bool]) -> torch.Tensor:
+    continuous_coords = (coords + 2 * torch.pi) - torch.Tensor([torch.pi if switch else 0 for switch in switch_coords])
+    switched_coords = torch.tensor(
+        [coord if coord <= torch.pi else -(2 * torch.pi - coord) for coord in continuous_coords])
 
     return switched_coords
 
 
-def to_other_intrinsic(intrinsic: np.ndarray) -> np.ndarray:
+def to_other_intrinsic(intrinsic: torch.Tensor) -> torch.Tensor:
     n = intrinsic.shape[0]
     total_charts = 2 ** intrinsic.shape[0]  # antipodal chart for each coordinate
 
-    intrinsic_charts = np.zeros((total_charts, n))
+    intrinsic_charts = torch.zeros((total_charts, n))
     for i, antipodal in enumerate(itertools.product([False, True], repeat=n)):
         intrinsic_charts[i, :] = _switch_antipodal_coords(intrinsic, antipodal)
 
     return intrinsic_charts
+
+
+def metric(intrinsic: torch.Tensor) -> torch.Tensor:
+    coord_jacs = jacobian(to_extrinsic, intrinsic, create_graph=True)
+    g = torch.tensordot(coord_jacs, coord_jacs, dims=([0], [0]))
+    return g
+
+
+def christoffels(intrinsic: torch.Tensor) -> torch.Tensor:
+    g = metric(intrinsic)
+    g_partials = jacobian(metric, intrinsic, create_graph=True)  # adds index at end due to partials
+
+    # computes the connection coefficients of the Levi-Civita connection using the metric thereby describing the
+    # curvature of the n-dimensional hypershere in the intrinsic coordinate system
+    conn_coeffs = 0.5 * torch.tensordot(g.inverse(), g_partials + torch.transpose(g_partials, 1, 2) - torch.transpose(
+        torch.transpose(g_partials, 1, 2), 0, 1), dims=([1], [0]))
+
+    return conn_coeffs
--- src/trajectory/test_mflds.py
+++ src/trajectory/test_mflds.py
@@ -1,121 +1,148 @@
 import unittest
 
-import numpy as np
-import numpy.testing as npt
+import torch
+import torch.testing as tt
 
 import trajectory.sn_mfld as sn_mfld
 
 
 def _unit(x):
-    return x / np.linalg.norm(x)
+    return x / torch.linalg.norm(x)
 
 
 class TestSnManifold(unittest.TestCase):
     def test_switch_antipodal_coords(self):
-        npt.assert_allclose(sn_mfld._switch_antipodal_coords(np.array([0.0]), [True]),
-                            np.array([np.pi]))
-        npt.assert_allclose(sn_mfld._switch_antipodal_coords(np.array([np.pi / 4]), [True]),
-                            np.array([-3.0 / 4 * np.pi]))
-        npt.assert_allclose(sn_mfld._switch_antipodal_coords(np.array([-np.pi / 4]), [True]),
-                            np.array([+3.0 / 4 * np.pi]))
-        npt.assert_allclose(sn_mfld._switch_antipodal_coords(np.array([0.0, 0.0]), [False, True]),
-                            np.array([0.0, np.pi]))
+        tt.assert_close(sn_mfld._switch_antipodal_coords(torch.tensor([0.0]), [True]),
+                        torch.Tensor([torch.pi]))
+        tt.assert_close(sn_mfld._switch_antipodal_coords(torch.Tensor([torch.pi / 4]), [True]),
+                        torch.Tensor([-3.0 / 4 * torch.pi]))
+        tt.assert_close(sn_mfld._switch_antipodal_coords(torch.Tensor([-torch.pi / 4]), [True]),
+                        torch.Tensor([+3.0 / 4 * torch.pi]))
+        tt.assert_close(sn_mfld._switch_antipodal_coords(torch.Tensor([0.0, 0.0]), [False, True]),
+                        torch.Tensor([0.0, torch.pi]))
 
     def test_intrinsic_extrinsic_transform(self):
         # S1 embedding into R2
         x_set = [
-            _unit(np.array([3.0, 4.0])),
-            _unit(np.array([-3.0, 4.0])),
-            _unit(np.array([3.0, -4.0])),
-            _unit(np.array([-3.0, -4.0])),
+            _unit(torch.Tensor([3.0, 4.0])),
+            _unit(torch.Tensor([-3.0, 4.0])),
+            _unit(torch.Tensor([3.0, -4.0])),
+            _unit(torch.Tensor([-3.0, -4.0])),
         ]
         for x in x_set:
-            npt.assert_allclose(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
+            tt.assert_close(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
 
         # S2 embedding into R3
         x_set = [
-            _unit(np.array([3.0, 4.0, 5.0])),
-            _unit(np.array([3.0, 4.0, -5.0])),
-            _unit(np.array([3.0, -4.0, 5.0])),
-            _unit(np.array([3.0, -4.0, -5.0])),
+            _unit(torch.Tensor([3.0, 4.0, 5.0])),
+            _unit(torch.Tensor([3.0, 4.0, -5.0])),
+            _unit(torch.Tensor([3.0, -4.0, 5.0])),
+            _unit(torch.Tensor([3.0, -4.0, -5.0])),
 
-            _unit(np.array([-3.0, 4.0, 5.0])),
-            _unit(np.array([-3.0, 4.0, -5.0])),
-            _unit(np.array([-3.0, -4.0, 5.0])),
-            _unit(np.array([-3.0, -4.0, -5.0])),
+            _unit(torch.Tensor([-3.0, 4.0, 5.0])),
+            _unit(torch.Tensor([-3.0, 4.0, -5.0])),
+            _unit(torch.Tensor([-3.0, -4.0, 5.0])),
+            _unit(torch.Tensor([-3.0, -4.0, -5.0])),
         ]
         for x in x_set:
-            npt.assert_allclose(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
+            tt.assert_close(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
 
         # S3 embedding into R4
         x_set = [
-            _unit(np.array([3.0, 4.0, 5.0, 6.0])),
-            _unit(np.array([3.0, 4.0, 5.0, -6.0])),
-            _unit(np.array([3.0, 4.0, -5.0, 6.0])),
-            _unit(np.array([3.0, 4.0, -5.0, -6.0])),
+            _unit(torch.Tensor([3.0, 4.0, 5.0, 6.0])),
+            _unit(torch.Tensor([3.0, 4.0, 5.0, -6.0])),
+            _unit(torch.Tensor([3.0, 4.0, -5.0, 6.0])),
+            _unit(torch.Tensor([3.0, 4.0, -5.0, -6.0])),
 
-            _unit(np.array([3.0, -4.0, 5.0, 6.0])),
-            _unit(np.array([3.0, -4.0, 5.0, -6.0])),
-            _unit(np.array([3.0, -4.0, -5.0, 6.0])),
-            _unit(np.array([3.0, -4.0, -5.0, -6.0])),
+            _unit(torch.Tensor([3.0, -4.0, 5.0, 6.0])),
+            _unit(torch.Tensor([3.0, -4.0, 5.0, -6.0])),
+            _unit(torch.Tensor([3.0, -4.0, -5.0, 6.0])),
+            _unit(torch.Tensor([3.0, -4.0, -5.0, -6.0])),
 
-            _unit(np.array([-3.0, 4.0, 5.0, 6.0])),
-            _unit(np.array([-3.0, 4.0, 5.0, -6.0])),
-            _unit(np.array([-3.0, 4.0, -5.0, 6.0])),
-            _unit(np.array([-3.0, 4.0, -5.0, -6.0])),
+            _unit(torch.Tensor([-3.0, 4.0, 5.0, 6.0])),
+            _unit(torch.Tensor([-3.0, 4.0, 5.0, -6.0])),
+            _unit(torch.Tensor([-3.0, 4.0, -5.0, 6.0])),
+            _unit(torch.Tensor([-3.0, 4.0, -5.0, -6.0])),
 
-            _unit(np.array([-3.0, -4.0, 5.0, 6.0])),
-            _unit(np.array([-3.0, -4.0, 5.0, -6.0])),
-            _unit(np.array([-3.0, -4.0, -5.0, 6.0])),
-            _unit(np.array([-3.0, -4.0, -5.0, -6.0])),
+            _unit(torch.Tensor([-3.0, -4.0, 5.0, 6.0])),
+            _unit(torch.Tensor([-3.0, -4.0, 5.0, -6.0])),
+            _unit(torch.Tensor([-3.0, -4.0, -5.0, 6.0])),
+            _unit(torch.Tensor([-3.0, -4.0, -5.0, -6.0])),
         ]
         for x in x_set:
-            npt.assert_allclose(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
+            tt.assert_close(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
 
         # S4 embedding into R5
         x_set = [
-            _unit(np.array([3.0, 4.0, 5.0, 6.0, 7.0])),
-            _unit(np.array([3.0, 4.0, 5.0, 6.0, -7.0])),
-            _unit(np.array([3.0, 4.0, 5.0, -6.0, 7.0])),
-            _unit(np.array([3.0, 4.0, 5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([3.0, 4.0, 5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([3.0, 4.0, 5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([3.0, 4.0, 5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([3.0, 4.0, 5.0, -6.0, -7.0])),
 
-            _unit(np.array([3.0, 4.0, -5.0, 6.0, 7.0])),
-            _unit(np.array([3.0, 4.0, -5.0, 6.0, -7.0])),
-            _unit(np.array([3.0, 4.0, -5.0, -6.0, 7.0])),
-            _unit(np.array([3.0, 4.0, -5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([3.0, 4.0, -5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([3.0, 4.0, -5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([3.0, 4.0, -5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([3.0, 4.0, -5.0, -6.0, -7.0])),
 
-            _unit(np.array([3.0, -4.0, 5.0, 6.0, 7.0])),
-            _unit(np.array([3.0, -4.0, 5.0, 6.0, -7.0])),
-            _unit(np.array([3.0, -4.0, 5.0, -6.0, 7.0])),
-            _unit(np.array([3.0, -4.0, 5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([3.0, -4.0, 5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([3.0, -4.0, 5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([3.0, -4.0, 5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([3.0, -4.0, 5.0, -6.0, -7.0])),
 
-            _unit(np.array([3.0, -4.0, -5.0, 6.0, 7.0])),
-            _unit(np.array([3.0, -4.0, -5.0, 6.0, -7.0])),
-            _unit(np.array([3.0, -4.0, -5.0, -6.0, 7.0])),
-            _unit(np.array([3.0, -4.0, -5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([3.0, -4.0, -5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([3.0, -4.0, -5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([3.0, -4.0, -5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([3.0, -4.0, -5.0, -6.0, -7.0])),
 
-            _unit(np.array([-3.0, 4.0, 5.0, 6.0, 7.0])),
-            _unit(np.array([-3.0, 4.0, 5.0, 6.0, -7.0])),
-            _unit(np.array([-3.0, 4.0, 5.0, -6.0, 7.0])),
-            _unit(np.array([-3.0, 4.0, 5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, 5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, 5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, 5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, 5.0, -6.0, -7.0])),
 
-            _unit(np.array([-3.0, 4.0, -5.0, 6.0, 7.0])),
-            _unit(np.array([-3.0, 4.0, -5.0, 6.0, -7.0])),
-            _unit(np.array([-3.0, 4.0, -5.0, -6.0, 7.0])),
-            _unit(np.array([-3.0, 4.0, -5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, -5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, -5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, -5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, 4.0, -5.0, -6.0, -7.0])),
 
-            _unit(np.array([-3.0, -4.0, 5.0, 6.0, 7.0])),
-            _unit(np.array([-3.0, -4.0, 5.0, 6.0, -7.0])),
-            _unit(np.array([-3.0, -4.0, 5.0, -6.0, 7.0])),
-            _unit(np.array([-3.0, -4.0, 5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, 5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, 5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, 5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, 5.0, -6.0, -7.0])),
 
-            _unit(np.array([-3.0, -4.0, -5.0, 6.0, 7.0])),
-            _unit(np.array([-3.0, -4.0, -5.0, 6.0, -7.0])),
-            _unit(np.array([-3.0, -4.0, -5.0, -6.0, 7.0])),
-            _unit(np.array([-3.0, -4.0, -5.0, -6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, -5.0, 6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, -5.0, 6.0, -7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, -5.0, -6.0, 7.0])),
+            _unit(torch.Tensor([-3.0, -4.0, -5.0, -6.0, -7.0])),
         ]
         for x in x_set:
-            npt.assert_allclose(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
+            tt.assert_close(sn_mfld.to_extrinsic(sn_mfld.to_intrinsic(x)), x)
+
+    def test_metric(self):
+        # S1 embedding into R2
+        x_intrinsic = sn_mfld.to_intrinsic(_unit(torch.Tensor([3.0, 4.0])))
+        tt.assert_close(sn_mfld.metric(x_intrinsic), torch.ones((1, 1)))
+
+        # S2 embedding into R3
+        x_intrinsic = sn_mfld.to_intrinsic(_unit(torch.Tensor([3.0, 4.0, 5.0])))
+        s2_metric = torch.tensor(
+            [
+                [1.0, 0.0],
+                [0.0, torch.sin(x_intrinsic[0])**2]
+            ])
+        tt.assert_close(sn_mfld.metric(x_intrinsic), s2_metric)
+
+    def test_christoffels(self):
+        # S1 embedding into R2
+        x_intrinsic = sn_mfld.to_intrinsic(_unit(torch.Tensor([3.0, 4.0])))
+        tt.assert_close(sn_mfld.christoffels(x_intrinsic), torch.zeros((1, 1, 1)))
+
+        # S2 embedding into R3
+        x_intrinsic = sn_mfld.to_intrinsic(_unit(torch.Tensor([3.0, 4.0, 5.0])))
+        s2_christoffels = torch.zeros((2,2,2))
+        s2_christoffels[0, 1, 1] = -torch.cos(x_intrinsic[0]) * torch.sin(x_intrinsic[0])
+        s2_christoffels[1, 0, 1] = 1.0 / torch.tan(x_intrinsic[0])
+        s2_christoffels[1, 1, 0] = s2_christoffels[1, 0, 1]
+        tt.assert_close(sn_mfld.christoffels(x_intrinsic), s2_christoffels)
 
 
 if __name__ == '__main__':
