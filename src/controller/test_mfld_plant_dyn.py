Fix incorrect distance calculation so fixed geodesics for real this time

src/controller/test_mfld_plant_dyn.py
@@ -35,13 +35,33 @@ def test_s1_dynamics(radius, p_extrinsic, q_extrinsic):
     tt.assert_close(torch.tensor(result.pos_extrinsic), q_extrinsic)
 
 
+s2_test_points = [
+    # quadrants
+    torch.tensor([1.0, 1.0, 1.0]),
+    torch.tensor([1.0, 1.0, -1.0]),
+    torch.tensor([1.0, -1.0, 1.0]),
+    torch.tensor([1.0, -1.0, -1.0]),
+
+    torch.tensor([-1.0, 1.0, 1.0]),
+    torch.tensor([-1.0, 1.0, -1.0]),
+    torch.tensor([-1.0, -1.0, 1.0]),
+    torch.tensor([-1.0, -1.0, -1.0]),
+
+    # axes
+    torch.tensor([1.0, 0.0, 0.0]),
+    torch.tensor([0.0, 1.0, 0.0]),
+    torch.tensor([0.0, 0.0, 1.0]),
+
+    torch.tensor([-1.0, 0.0, 0.0]),
+    torch.tensor([0.0, -1.0, 0.0]),
+    torch.tensor([0.0, 0.0, -1.0])
+]
+
+
 @pytest.mark.parametrize("radius, p_extrinsic, q_extrinsic",
-                         itertools.product([1.0, 2.0, 0.5],
-                                           [torch.tensor([1.0, 0.0, 0.0]), torch.tensor([1.0, 0.0, 0.0]),
-                                            torch.tensor([0.0, 1.0, 0.0]), torch.tensor([0.0, 0.0, 1.0]),
-                                            torch.tensor([-1.0, 0.0, 0.0]), torch.tensor([0.0, -1.0, 0.0]),
-                                            torch.tensor([0.0, 0.0, -1.0])],
-                                           [torch.tensor([1.0, 1.0, 1.0]), torch.tensor([0.0, 1.0, 0.0])]))
+                         itertools.product([1.0],
+                                           s2_test_points,
+                                           s2_test_points))
 def test_s2_dynamics(radius, p_extrinsic, q_extrinsic):
     s2 = sn_mfld.HypersphereManifold(2, radius)
 
@@ -110,16 +130,22 @@ def test_s3_dynamics(radius, p_extrinsic, q_extrinsic):
     # better architecture could be used but that's not needed for this project)
     tt.assert_close(torch.tensor(result.pos_extrinsic), q_extrinsic, rtol=1E-4, atol=1E-5)
 
+
 @pytest.mark.parametrize("radius, p_extrinsic, q_extrinsic",
                          itertools.product([1.0, 2.0, 0.5],
-                                           [torch.tensor([1.0, 0.0, 0.0, 0.0, 0.0]), torch.tensor([0.0, 1.0, 0.0, 0.0, 0.0]),
-                                            torch.tensor([0.0, 0.0, 1.0, 0.0, 0.0]), torch.tensor([0.0, 0.0, 0.0, 1.0, 0.0]),
+                                           [torch.tensor([1.0, 0.0, 0.0, 0.0, 0.0]),
+                                            torch.tensor([0.0, 1.0, 0.0, 0.0, 0.0]),
+                                            torch.tensor([0.0, 0.0, 1.0, 0.0, 0.0]),
+                                            torch.tensor([0.0, 0.0, 0.0, 1.0, 0.0]),
                                             torch.tensor([0.0, 0.0, 0.0, 0.0, 1.0]),
-                                            torch.tensor([-1.0, 0.0, 0.0, 0.0, 0.0]), torch.tensor([0.0, -1.0, 0.0, 0.0, 0.0]),
-                                            torch.tensor([0.0, 0.0, -1.0, 0.0, 0.0]), torch.tensor([0.0, 0.0, 0.0, -1.0, 0.0]),
+                                            torch.tensor([-1.0, 0.0, 0.0, 0.0, 0.0]),
+                                            torch.tensor([0.0, -1.0, 0.0, 0.0, 0.0]),
+                                            torch.tensor([0.0, 0.0, -1.0, 0.0, 0.0]),
+                                            torch.tensor([0.0, 0.0, 0.0, -1.0, 0.0]),
                                             torch.tensor([0.0, 0.0, 0.0, 0.0, -1.0]),
                                             ],
-                                           [torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0]), torch.tensor([0.0, 1.0, 0.0, 0.0, 0.0])]))
+                                           [torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0]),
+                                            torch.tensor([0.0, 1.0, 0.0, 0.0, 0.0])]))
 def test_s4_dynamics(radius, p_extrinsic, q_extrinsic):
     s4 = sn_mfld.HypersphereManifold(4, radius)
 
@@ -149,6 +175,7 @@ def test_s4_dynamics(radius, p_extrinsic, q_extrinsic):
     # NOTE: there are so many floating point operations that long term geodesics are hard to compute (theoretically a
     # better architecture could be used but that's not needed for this project)
     tt.assert_close(torch.tensor(result.pos_extrinsic), q_extrinsic, rtol=1E-4, atol=1E-5)
+
 
 def test_stuff():
     s4 = sn_mfld.HypersphereManifold(4, 1.0)

src/controller/transport_cntrlr.py
@@ -115,19 +115,13 @@ class TransportPDController(TransportController):
         state_pos, state_vel = state
         target_vel = target_ts_transp_to_state[0]
 
-        metric = self._dynamics.manifold.metric(chart, torch.tensor(state_pos,
-                                                                    dtype=torch.get_default_dtype())).detach().numpy()
+        state_pos_tensor = torch.tensor(state_pos, dtype=torch.get_default_dtype())
+        metric = self._dynamics.manifold.metric(chart, state_pos_tensor).detach().numpy()
+        christoffels = self._dynamics.manifold.christoffels(chart, state_pos_tensor).detach().numpy()
 
-        print("GENERATE TRANSPORT CONTROLS")
-        print(f"chart: {chart}")
-        print(f"target_vel: {target_vel}")
-        print(f"state_vel: {state_vel}")
-        print(f"kp gains: {self._kp_gains}")
-        print(f"kd gains: {self._kd_gains}")
-
-        print(f"riem_log: {riem_log}")
-        print(f"target_vel: {target_vel}")
-
-        controls = self._kp_gains @ metric @ riem_log + self._kd_gains @ metric @ (target_vel - state_vel)
-        print(f"controls: {controls}")
+        # performs feedback linearization to cancel out the natural acceleration of the geodesic and therefore the
+        # evolution of the dynamics behaves like a linear system
+        geod_accel = -np.tensordot(np.tensordot(christoffels, state_vel, ([2], [0])), state_vel, ([1], [0]))
+        controls = (self._kp_gains @ riem_log + self._kd_gains @ (target_vel - state_vel)) - geod_accel
+        # print(f"controls: {controls}")
         return controls

src/manifolds/sn_mfld.py
@@ -191,6 +191,8 @@ def to_intrinsic_ts(extrinsic: torch.Tensor, extrinsic_ts: torch.Tensor, chart_i
     intrinsic = to_intrinsic(extrinsic, chart_idx)
     ts_basis_in_extrinsic = _intrinsic_ts_basis_in_extrinsic(intrinsic, chart_idx, radius)
 
+    _check_ts_basis(chart_idx, ts_basis_in_extrinsic)
+
     # print(f"intrinsic: {intrinsic}")
     # print(f"ts_basis_in_extrinsic: {ts_basis_in_extrinsic}")
 
@@ -203,18 +205,29 @@ def to_intrinsic_ts(extrinsic: torch.Tensor, extrinsic_ts: torch.Tensor, chart_i
     basis_dot = torch.diag(torch.tensordot(
         ts_basis_in_extrinsic, ts_basis_in_extrinsic, dims=([0], [0])))
 
-    # print(f"basis_dot: {basis_dot}")
-
     intrinsic_ts = vec_dot_with_basis / basis_dot
+
+    recon_extrinsic_ts = to_extrinsic_ts(intrinsic, intrinsic_ts, chart_idx, radius)
+    if torch.linalg.norm(recon_extrinsic_ts - extrinsic_ts) > ZERO_NORM_EPS:
+        raise ValueError("Provided extrinsic vector is not within the extrinsic tangent space")
 
     # print(f"intrinsic_ts: {intrinsic_ts}")
 
     return intrinsic_ts
 
 
+def _check_ts_basis(chart_idx: int, ts_basis_in_extrinsic: torch.Tensor):
+    # will fail explicitly
+    n = ts_basis_in_extrinsic.shape[1]
+    if torch.linalg.matrix_rank(ts_basis_in_extrinsic) < n:
+        raise ValueError(f"Tangent basis is rank-deficient, use a different chart other than id={chart_idx}")
+
+
 def to_extrinsic_ts(intrinsic: torch.Tensor, intrinsic_ts: torch.Tensor, chart_idx: int,
                     radius: float = 1.0) -> torch.Tensor:
     ts_basis_in_extrinsic = _intrinsic_ts_basis_in_extrinsic(intrinsic, chart_idx, radius)
+
+    _check_ts_basis(chart_idx, ts_basis_in_extrinsic)
 
     # scales the basis vectors (columns of the extrinsic basis) by the intrinsic coordinates
     extrinsic_vec = torch.tensordot(intrinsic_ts, ts_basis_in_extrinsic, dims=([0], [1]))
@@ -230,12 +243,22 @@ def project_extrinsic_vec_onto_ts(extrinsic_vec: torch.Tensor, chart_idx: int, e
     intrinsic = to_intrinsic(extrinsic, chart_idx)
     ts_basis_in_extrinsic = _intrinsic_ts_basis_in_extrinsic(intrinsic, chart_idx, radius)
 
+    _check_ts_basis(chart_idx, ts_basis_in_extrinsic)
+
+    # print(f"ts_basis_in_extrinsic: {ts_basis_in_extrinsic}")
+
     # projects the Euclidean vector onto the basis of the tangent space
     dot_extrinsic_with_basis = torch.tensordot(extrinsic_vec, ts_basis_in_extrinsic, dims=([0], [0]))
     dot_basis = torch.diag(torch.tensordot(ts_basis_in_extrinsic, ts_basis_in_extrinsic, dims=([0], [0])))
 
+    # print(f"dot_extrinsic_with_basis: {dot_extrinsic_with_basis}")
+    # print(f"dot_basis: {dot_basis}")
+
     factor_on_basis = dot_extrinsic_with_basis / dot_basis
     scaled_basis = torch.tensordot(factor_on_basis, ts_basis_in_extrinsic, dims=([0], [1]))
+
+    # print(f"factor_on_basis: {factor_on_basis}")
+    # print(f"scaled_basis: {scaled_basis}")
 
     return scaled_basis
 
@@ -296,6 +319,8 @@ def _to_all_intrinsic_ts(extrinsic: torch.Tensor, extrinsic_ts: torch.Tensor, ra
 def distance(p_intrinsic: torch.Tensor, q_intrinsic, chart_idx: int, radius: float = 1.0) -> float:
     p_extrinsic = to_extrinsic(p_intrinsic, chart_idx, radius)
     q_extrinsic = to_extrinsic(q_intrinsic, chart_idx, radius)
+
+    # print(f"inside distance, p_extrinsic: {p_extrinsic}, q_extrinsic: {q_extrinsic}")
 
     # computes the distance by first computing the angle between the points in the intrinsic space then computes the
     # distance by evaluating the arc length of the hypersphere
@@ -412,13 +437,15 @@ class HypersphereManifold(ManifoldCoordSystem):
         p_extrinsic = self.to_extrinsic(chart, p)
         q_extrinsic = self.to_extrinsic(chart, q)
 
+        # print(f"p_extrinsic: {p_extrinsic}, q_extrinsic: {q_extrinsic}")
+
         # manual check given the norm will be 0 in this case which will lead to a nan result
         if torch.allclose(p_extrinsic, q_extrinsic):
             return torch.zeros(p.shape)
 
         d_extrinsic = q_extrinsic - p_extrinsic
 
-        print(f"d_extrinsic: {d_extrinsic}")
+        # print(f"d_extrinsic: {d_extrinsic}")
 
         # projects onto the local tangent space if it exists, if d is orthogonal then the resulting point is at the
         # opposite side of the sphere so we just choose any vector in the tangent space
@@ -426,18 +453,15 @@ class HypersphereManifold(ManifoldCoordSystem):
                                                                  self._chart_nums[chart],
                                                                  p_extrinsic,
                                                                  self._radius)
-        print(f"proj norm: {torch.linalg.norm(d_proj_ts_at_p_extrinsic)}")
+        # print(f"proj norm: {torch.linalg.norm(d_proj_ts_at_p_extrinsic)}")
         if torch.linalg.norm(d_proj_ts_at_p_extrinsic) < ZERO_NORM_EPS:
             d_proj_ts_at_p_extrinsic = self.to_extrinsic_ts(chart, p, torch.ones((self.n,)))
 
-        print(f"d_proj_ts_at_p_extrinsic: {d_proj_ts_at_p_extrinsic}")
+        distance_extrinsic = distance(p, q, self._chart_nums[chart], self._radius)
+        d_proj_ts_at_p_extrinsic /= torch.linalg.norm(d_proj_ts_at_p_extrinsic)
+        d_proj_ts_at_p_extrinsic *= distance_extrinsic
 
-        v_intrinsic = self.to_intrinsic_ts(chart, p_extrinsic, d_proj_ts_at_p_extrinsic)
-        dist = distance(p, q, self._chart_nums[chart], 1.0)  # distance is only scaled when moving to extrinsic coords
-        log = dist * v_intrinsic / torch.linalg.norm(v_intrinsic)
-
-        # print(f"log: {log}")
-
+        log = self.to_intrinsic_ts(chart, p_extrinsic, d_proj_ts_at_p_extrinsic)
         return log
 
     def transport_from_q(self, chart_p: str, p_intrinsic: torch.Tensor, chart_q: str, q_intrinsic: torch.Tensor,